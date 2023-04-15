506be360cf4aa74449fb32df769ee557ee0aed1e
Harshita <chandaliyaharshita222@gmail.com>
2023-04-15 20:02:55 +0530

    Checking Quotient and Remainder

--- Basic Core Problems/Program.cs
+++ Basic Core Problems/Program.cs
@@ -8,7 +8,7 @@ namespace Basic_Core_Problems
         {
             Console.WriteLine("Welcome to Basic Core Problems!");
             Console.WriteLine("Please choose any one program from below options:");
-            Console.WriteLine("1.Flip Coin\n2.Leap Year\n3.Power of 2\n4.Harmonic Number\n5.Factors");
+            Console.WriteLine("1.Flip Coin\n2.Leap Year\n3.Power of 2\n4.Harmonic Number\n5.Factors\n6.Division");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -32,6 +32,10 @@ namespace Basic_Core_Problems
                 case 5:
                     Factors factors = new Factors();
                     factors.fact();
+                    break;
+                case 6:
+                    Division division = new Division();
+                    division.divide();
                     break;
             }
         }
