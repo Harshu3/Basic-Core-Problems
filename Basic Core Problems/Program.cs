2ca00e8d49ff05a4eaabac6f6fcf174e712e33ad
Harshita <chandaliyaharshita222@gmail.com>
2023-04-15 21:31:33 +0530

    Adding Files

--- Basic Core Problems/Program.cs
+++ Basic Core Problems/Program.cs
@@ -8,8 +8,36 @@ namespace Basic_Core_Problems
         {
             Console.WriteLine("Welcome to Basic Core Problems!");
             Console.WriteLine("Please choose any one program from below options:");
-                    Swap2Nums swapNums = new Swap2Nums();
-                    swapNums.swap();
+            Console.WriteLine("1.Flip Coin\n2.Leap Year\n3.Power of 2\n4.Harmonic Number\n5.Factors\n6.Division");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    FlipCoin fc = new FlipCoin();
+                    fc.flipCoin();
+                    break;
+                case 2:
+                    LeapYear ly = new LeapYear();
+                    ly.Year();
+                    break;
+                case 3:
+                    PowerOf2 powerOf2 = new PowerOf2();
+                    powerOf2.powOf2();
+                    break;
+                case 4:
+                    HarmonicNumber harmonicNumber = new HarmonicNumber();
+                    harmonicNumber.HarmonicNum();
+                    break;
+                case 5:
+                    Factors factors = new Factors();
+                    factors.fact();
+                    break;
+                case 6:
+                    Division division = new Division();
+                    division.divide();
+                    break;
+            }
         }
     }
 }
