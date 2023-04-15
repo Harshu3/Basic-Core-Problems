a0783bda76c047f552ffce0a348b5d03562bb8b4
Harshita <chandaliyaharshita222@gmail.com>
2023-04-15 22:15:36 +0530

    Harmonic Number

--- Basic Core Problems/Program.cs
+++ Basic Core Problems/Program.cs
@@ -7,28 +7,9 @@ namespace Basic_Core_Problems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Core Problems!");
-            Console.WriteLine("Please choose any one program from below options:");
-            Console.WriteLine("1.Flip Coin\n2.Leap Year\n3.Power of 2\n4.Harmonic Number");
-            int option = Convert.ToInt32(Console.ReadLine());
-
-            switch (option)
-            {
-                case 1:
-                    FlipCoin fc = new FlipCoin();
-                    fc.flipCoin();
-                    break;
-                case 2:
-                    LeapYear ly = new LeapYear();
-                    ly.Year();
-                    break;
-                case 3:
-                    PowerOf2 powerOf2 = new PowerOf2();
-                    powerOf2.powOf2();
-                    break;
-                case 4:
+            Console.WriteLine("Displaying Harmonic Number");
                     HarmonicNumber harmonicNumber = new HarmonicNumber();
                     harmonicNumber.HarmonicNum();
-                    break;
             }
         }
     }
