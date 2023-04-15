e3bde894663ffd3450d6f3134742a5f01b4f60cf
Harshita <chandaliyaharshita222@gmail.com>
2023-04-15 19:20:24 +0530

    Checking Leap Year or not

--- Basic Core Problems/Program.cs
+++ Basic Core Problems/Program.cs
@@ -7,20 +7,9 @@ namespace Basic_Core_Problems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Core Problems!");
-            Console.WriteLine("Please choose any one program from below options:");
-            Console.WriteLine("1.Flip Coin\n2.Leap Year");
-            int option = Convert.ToInt32(Console.ReadLine());
-
-            switch (option)
-            {
-                case 1:
-                    FlipCoin fc = new FlipCoin();
-                    fc.flipCoin();
-                    break;
-                case 2:
+            Console.WriteLine("Checking Leap Year or not");
                     LeapYear ly = new LeapYear();
                     ly.Year();
-                    break;
             }
         }
     }
