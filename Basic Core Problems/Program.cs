e22c6bc39f0c9b523f65d3e346bc3b448afe0282
Harshita <chandaliyaharshita222@gmail.com>
2023-04-15 16:37:21 +0530

    Flipping a Coin

---
+++ Basic Core Problems/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Basic_Core_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Core Problems!");
+            Console.WriteLine("Flipping a Coin");
+            FlipCoin fc = new FlipCoin();
+                    fc.flipCoin();
+        }
+    }
+}
